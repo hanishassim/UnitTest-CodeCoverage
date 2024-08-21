3b9e3751eeb57249d1c4e5fc2b8a654574a9ff8d
hanishassim <4571502+hanishassim@users.noreply.github.com>
2024-08-21 09:47:47 +0800

    Initial commit

---
+++ CodeCoverage/CoveredClass.swift
@@ -0,0 +1,18 @@
+//
+//  CoveredClass.swift
+//  CodeCoverage
+//
+//  Created by Hassim, Muhammad Hanis on 21/08/2024.
+//
+
+import UIKit
+
+class CoveredClass {
+    static func max(_ x: Int, _ y: Int) -> Int {
+        if x < y {
+            return y
+        } else {
+            return x
+        }
+    }
+}
---
+++ CodeCoverageTests/CoveredClassTests.swift
@@ -0,0 +1,15 @@
+//
+//  CoveredClassTests.swift
+//  CodeCoverageTests
+//
+//  Created by Hassim, Muhammad Hanis on 21/08/2024.
+//
+
+import XCTest
+@testable import CodeCoverage
+
+final class CoveredClassTests: XCTestCase {
+    func test_zero() {
+        XCTFail("Tests not yet implemented in CoveredClassTests")
+    }
+}
