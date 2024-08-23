c4eb520ea934ae155d1ce676b2251fb1f69f159c
hanishassim <4571502+hanishassim@users.noreply.github.com>
2024-08-23 11:40:21 +0800

    Add assertion to unit test

--- CodeCoverageTests/CoveredClassTests.swift
+++ CodeCoverageTests/CoveredClassTests.swift
@@ -9,7 +9,9 @@ import XCTest
 @testable import CodeCoverage
 
 final class CoveredClassTests: XCTestCase {
-    func test_max_with1And2_shouldReturnSomething() {
+    func test_max_with1And2_shouldReturn2() {
         let result = CoveredClass.max(1, 2)
+        
+        XCTAssertEqual(result, 2)
     }
 }
